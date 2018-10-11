414c93d2438932dc6ffc4c2643a009c967341942
Cetin Dennis <1810653806@stud.fh-kufstein.ac.at>
2018-10-11 15:41:11 +0200

    dritter commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
         String vorname = " Dennis";
         int alter = 25;
+        String nachname = " cetin";
 
-        System.out.println("Hello" + vorname + " du bist " + alter + " Jahre jung" +  "!");
+
+
+        System.out.println("Hello" + vorname + nachname + " du bist " + alter + " Jahre jung" +  "!");
     }
 }
