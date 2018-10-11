c7ee5d1f1e5719d19a386422c8f753c33032e976
Cetin Dennis <1810653806@stud.fh-kufstein.ac.at>
2018-10-11 15:46:57 +0200

    fünfter commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ public class Main {
     public static void main(String[] args) {
         String vorname = " Dennis";
         int alter = 25;
-        String nachname = " cetin";
-        String herkunft = "deutschland";
+        String nachname = " Cetin";
+        String herkunft = "Deutschland";
+        String jahrgang = " 1993";
 
 
 
-        System.out.println("Hello" + vorname + nachname + " du kommst aus " + herkunft + " und du bist " + alter + " Jahre jung" +  "!");
+        System.out.println("Hello" + vorname + nachname + " du kommst aus " + herkunft + " und geboren im Jahr" + jahrgang +  " und du bist " + alter + " Jahre jung" +  "!");
     }
 }
